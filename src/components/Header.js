312a3db85c1e99e8a9f50b14b20f54ceb8a897fc
Calvin Lee <Anthony2006Lee@hotmail.com>
2022-03-08 00:03:02 -0600

    mild changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import smiles from "../images/smiles.jpg"
+import { Button } from 'react-bootstrap'
+
 
 export default function Header() {
     return (
-        <div>
+        <div >
+
+
+
             <img src={smiles}
                 alt="picture of me smiling"
                 className="smilePic" />
 
-            <h1 className="name">Calvin A. Lee</h1>
-            <p className="title">Software Developer</p>
-            <p className="email">CalvinALee2006@hotmail.com</p>
+            <h1 className="Name">Calvin A. Lee</h1>
+            <p className="Title">Software Developer</p>
+
+            <a href="Calvinalee2006@hotmail.com" target="_blank" className="Email">
+                <Button>E- </Button>
+            </a>
+
+            <a href="https://www.linkedin.com/in/calvin-lee-90082006/" target="_blank" className="LinkdIn">
+                <Button> LinkedIn </Button>
+            </a>
 
         </div>
     )
